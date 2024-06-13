8f20eae82aebc47d95448a7dfa658321dce66bb5
PrinMax <max.prin@edu.univ-fcomte.fr>
2024-06-13 16:44:43 +0200

    changement d'une petite erreur concernant les imagres+ fin de la page avec début footer

--- src/components/footer.vue
+++ src/components/footer.vue
@@ -1,7 +1,24 @@
 <!-- eslint-disable vue/multi-word-component-names -->
 <script setup lang="ts">
-
+import logo from './icons/logo.vue';
+import Facebook from './icons/Facebook.vue';
+import insta from './icons/insta.vue';
+import twitter from './icons/twitter.vue';
 </script>
 <template>
-    <p>Footer</p>
+    <div>
+        <footer class="bg-[#519DC8]">
+            <div class="flex h-20 text-white">
+                <logo class="ml-2 flex"/>
+                <div>
+                    <p>À propos</p>
+                </div>
+                <div class="flex ml-[30px] h-10 mt-2">
+                    <Facebook class="ml-2"/>
+                    <insta class="ml-2"/>
+                    <twitter class="ml-2"/>
+                </div>
+            </div>
+        </footer>
+    </div>
 </template>
--- src/pages/index.vue
+++ src/pages/index.vue
@@ -9,7 +9,7 @@ import flèche from '@/components/icons/flèche.vue'
   <body class="bg-[#DDDDDD]">
   <div style="width: 100%; ">
     <div style="z-index: 1;">
-      <img src="C:\Users\Max88\Desktop\ProjetS2\src\components\img\image 1 (1) 1.webp" alt="image d'accueil">
+      <img src="/src/components/img/image 1 (1) 1.webp" alt="image d'accueil">
     </div>
     <div style="position: absolute;top: 300px;width: 650px; height: 400px; z-index: 2; font-size: 200%; margin-left: 450px;">
       <div class="font-body-text">
@@ -25,30 +25,36 @@ import flèche from '@/components/icons/flèche.vue'
         <div class="min-w-full flex-shrink-0 snap-center">
             <div class="bg-white rounded-lg shadow-lg p-6 flex">
                 <h2 class="text-2xl font-bold underline-offset-1 ml-[70px] place-content-center"  >Intelligence artificielle : Sam Altman réintègre le conseil d'administration d'OpenAI</h2>
-                <img src="C:\Users\Max88\Desktop\ProjetS2\src\components\img\image 2.webp" alt="image du 1er article" width="10%" height="10%" class="ml-[70px]">
+                <img src="/src/components/img/image 2.webp" alt="image du 1er article" width="10%" height="10%" class="ml-[70px]">
             </div>
         </div>
         <div class="min-w-full flex-shrink-0 snap-center">
             <div class="bg-white rounded-lg shadow-lg p-6 flex">
                 <h2 class="text-2xl font-bold underline-offset-1 ml-[70px] place-content-center">L'intelligence artificielle menace-t-elle l'environnement ?</h2>
-                <img src="C:\Users\Max88\Desktop\ProjetS2\src\components\img\image 3.webp" alt="image du 2eme article" width="10%" height="10%" class="ml-[150px]">
+                <img src="/src/components/img/image 3.webp" alt="image du 2eme article" width="10%" height="10%" class="ml-[150px]">
             </div>
         </div>
         <div class="min-w-full flex-shrink-0 snap-center">
             <div class="bg-white rounded-lg shadow-lg p-6 flex">
                 <h2 class="text-2xl font-bold underline-offset-1 ml-[70px] place-content-center">Qu'est-ce que la Green IT ?</h2>
-                <img src="C:\Users\Max88\Desktop\ProjetS2\src\components\img\image 4.webp" alt="image du 3eme article" width="10%" height="10%" class="ml-[400px]">
+                <img src="/src/components/img/image 4.webp" alt="image du 3eme article" width="10%" height="10%" class="ml-[400px]">
             </div>
         </div>
                 <div class="min-w-full flex-shrink-0 snap-center">
             <div class="bg-white rounded-lg shadow-lg p-6 flex">
                 <h2 class="text-2xl font-bold underline-offset-1 ml-[70px] place-content-center">Selon IBM, l'informatique quantique pourrait détruire la cybersécurité des pays</h2>
-                <img src="C:\Users\Max88\Desktop\ProjetS2\src\components\img\image 5.webp" alt="image du 4eme article" width="10%" height="10%" class="ml-[250px]">
+                <img src="/src/components/img/image 5.webp" alt="image du 4eme article" width="10%" height="10%" class="ml-[250px]">
             </div>
         </div>
     </div>
-<div>
-  <p class="bg-[#C2E9FF] line-clamp-3">Avec Student’Book retrouvez toutes les actualités regroupées en un seul endroit et sur différents domaines afin de parfaire votre veille.</p>
+<div class="flex ml-5 mr-5">
+  <p class="text-xl bg-[#C2E9FF] h-20">Avec Student’Book retrouvez toutes les actualités regroupées en un <br> seul  endroit et sur différents domaines afin de parfaire votre veille. <br>Accéder aux actualités</p>
+  
+  <img class="w-[300px] h-[150px] items-center ml-5" src="/src/components/img/image 7.webp" alt="image actualités" width="10%" height="10%">
+</div>
+<div class="flex ml-5 mr-5">
+  <img class="w-[300px] h-[200px] items-center" src="/src/components/img/image 8.webp" alt="image newsletter">
+  <p class="bg-[#C2E9FF] h-20 text-xl">Abonnez vous à notre Newsletter pour suivre <br> les actualités de Student’Book.</p>
 </div>
 </body>
 
